e9b89823f1f59bceb33ae91d6dbcd02f538bb24e
Ashish Patel <toaashishpatel@gmail.com>
2020-02-24 02:02:36 +0530

    Added S3 Configuration and app settings

---
+++ src/AwsS3Labs.Web/Configuration/S3Config.cs
@@ -0,0 +1,16 @@
+﻿#region Imports
+#endregion
+
+namespace AwsS3Labs.Web.Configuration
+{
+    public class S3Config
+    {
+        public string BucketName { get; set; }
+
+        public string AWSRegion { get; set; }
+
+        public string AWSAccessKey { get; set; }
+
+        public string AWSSecretKey { get; set; }
+    }
+}
